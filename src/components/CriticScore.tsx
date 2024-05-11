ba0a42f351c5e6485e3a4cc19e7c0c0edb4c0980
irashidlatif <alisagar0107@gmail.com>
2024-05-11 11:50:49 +0500

    display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <div>
+      <Badge
+        colorScheme={color}
+        fontSize={"14px"}
+        paddingX={2}
+        borderRadius={"5px"}
+      >
+        {score}
+      </Badge>
+    </div>
+  );
+};
+
+export default CriticScore;
